f2bea8b12715baedea47fbbacb6326b8b9ec90c9
Soolking <souleymanetahiroumounkaila2025@gmail.com>
2026-09-26 09:36:22 +0100

    verification

--- Untitled-1.py
+++
@@ -1,6 +0,0 @@
-
-import requests
-ville = input("Entrer le nom d'une ville: ")
-temp = requests.get(f"https://wttr.in/{ville}?format=%t").text
-print(f"ville:{ville}\ntemperature actuelle:{temp}")
-
